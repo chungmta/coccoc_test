b5aa425cb24969154b7623be198ddaab113ea667
@minimize <chungnt@sonek.vn>
2023-01-05 17:07:17 +0700

    ci: update readme and script

--- README.md
+++ README.md
@@ -1,5 +1,34 @@
+## Prerequisites
+
+- Node.js >= 14
+- npm or yarn (package manager)
+
+## Getting Started
+1. Install dependencies
+
+```js
+npm run install-deps
+```
+
+- Or you can navigate to each directory and run the npm install command in each one.
 
 
+3. Start the frontend and backend servers
+
+```js
+npm run start
+```
+This will start both the frontend (port 3000) and backend (port 9000) concurrently.
+
+
+## Code-base
+This project is divided into three main directories: frontend, backend and scripts. 
+
+- frontend: contains the code for the client-side of the application.
+    + Created with ***create-react-app***, the output of the build process is a bundle of JavaScript files that use ES5 syntax.
+- backend: contains the code for the server-side of the application.
+    + I use nestjs to create an api that handles user authentication with JWT.
+- scripts: contains various js scripts.
 
 ---
 Have a good day!
--- scripts/install-dependencies.js
+++ scripts/install-dependencies.js
@@ -1,10 +1,21 @@
+/**
+ * This script install current dependencies
+ * and backend, frontend directory
+ */
 const { execSync } = require('child_process');
 
-const installDependencies = (directory) => {
-  execSync(`cd ${directory} && npm install`, { stdio: 'inherit' });
-}
+const installDependencies = (folder) => {
+  try {
+    console.log(`Installing dependencies for ${folder}...`);
+    execSync(`cd ${folder} && npm install`, { stdio: 'inherit' });
+    console.log(`Dependencies installed for ${folder}`);
+  } catch (error) {
+    console.error(`Error installing dependencies for ${folder}: ${error}`);
+  }
+};
 
-installDependencies('frontend');
-installDependencies('backend');
+installDependencies('.'); // install dependencies for parent folder
+installDependencies('frontend'); // install dependencies for frontend folder
+installDependencies('backend'); // install dependencies for backend folder
 
-console.log('Dependencies installed for both frontend and backend.');
+console.log('All dependencies installed!');
